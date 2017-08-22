ea026bc46ea9c7fdac81bf0574d02be7459e0d88
Dan Fishgold <d.fishgold@gmail.com>
2017-08-22 16:12:06 +0300

    Added CORS support for elm-live

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient
 const express = require('express')
-
+const cors = require('cors')
 
 const mongoUrl = 'mongodb://localhost:27017/photos-project'
 const mongoCollection = 'photos'
@@ -10,7 +10,8 @@ MongoClient.connect(mongoUrl, (err, db) => {
     const collection = db.collection(mongoCollection)
 
     const app = express()
-
+    app.use(cors())
+    
     app.get('/photos', function (req, res) {
 
         collection.find({}).toArray((err, docs) => {
